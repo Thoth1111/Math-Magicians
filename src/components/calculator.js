ab9f0387d72c5b73de86cbc05a2a4d78565adc23
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-15 13:52:36 +0300

    Render and style calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,44 +2,39 @@ import React from 'react';
 import '../App.css';
 
 const Calculator = () => (
-  <section>
-    <table className="calc-UI">
-      <tbody>
-        <tr>
-          <td colSpan="4"><input type="text" id="display" /></td>
-        </tr>
-        <tr>
-          <td><button type="button">AC</button></td>
-          <td><button type="button">+/-</button></td>
-          <td><button type="button">%</button></td>
-          <td><button type="button">&divide;</button></td>
-        </tr>
-        <tr>
-          <td><button type="button">7</button></td>
-          <td><button type="button">8</button></td>
-          <td><button type="button">9</button></td>
-          <td><button type="button">x</button></td>
-        </tr>
-        <tr>
-          <td><button type="button">4</button></td>
-          <td><button type="button">5</button></td>
-          <td><button type="button">6</button></td>
-          <td><button type="button">-</button></td>
-        </tr>
-        <tr>
-          <td><button type="button">1</button></td>
-          <td><button type="button">2</button></td>
-          <td><button type="button">3</button></td>
-          <td><button type="button">+</button></td>
-        </tr>
-        <tr>
-          <td colSpan="2"><button type="button">0</button></td>
-          <td><button type="button">.</button></td>
-          <td><button type="button">=</button></td>
-        </tr>
-      </tbody>
-    </table>
-
+  <section className="calc-UI">
+    <div className="keys">
+      <input type="text" id="display" />
+    </div>
+    <div className="keys">
+      <button type="button" className="calc-Btns">AC</button>
+      <button type="button" className="calc-Btns">+/-</button>
+      <button type="button" className="calc-Btns">%</button>
+      <button type="button" className="calc-Btns">&divide;</button>
+    </div>
+    <div className="keys">
+      <button type="button" className="calc-Btns">7</button>
+      <button type="button" className="calc-Btns">8</button>
+      <button type="button" className="calc-Btns">9</button>
+      <button type="button" className="calc-Btns">x</button>
+    </div>
+    <div className="keys">
+      <button type="button" className="calc-Btns">4</button>
+      <button type="button" className="calc-Btns">5</button>
+      <button type="button" className="calc-Btns">6</button>
+      <button type="button" className="calc-Btns">-</button>
+    </div>
+    <div className="keys">
+      <button type="button" className="calc-Btns">1</button>
+      <button type="button" className="calc-Btns">2</button>
+      <button type="button" className="calc-Btns">3</button>
+      <button type="button" className="calc-Btns">+</button>
+    </div>
+    <div className="keys">
+      <button type="button" className="calc-Btns">0</button>
+      <button type="button" className="calc-Btns">.</button>
+      <button type="button" className="calc-Btns">=</button>
+    </div>
   </section>
 );
 
