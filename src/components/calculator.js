f1450c94aa34597896743ce663b5307f2c5beaf3
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-16 09:33:34 +0300

    Modify calculator component to add event handlers

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,44 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
-// import operate from '../logic/operate';
-// import calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 import CalcButton from './calc-button';
 
-const Calculator = () => (
-  <section className="calc-UI">
-    <div className="keys">
-      <input type="text" id="display" />
-    </div>
-    <div className="keys">
-      <CalcButton value="AC" />
-      <CalcButton value="+" />
-      <CalcButton value="%" />
-      <CalcButton value="&" />
-    </div>
-    <div className="keys">
-      <CalcButton value="7" />
-      <CalcButton value="8" />
-      <CalcButton value="9" />
-      <CalcButton value="x" />
-    </div>
-    <div className="keys">
-      <CalcButton value="4" />
-      <CalcButton value="5" />
-      <CalcButton value="6" />
-      <CalcButton value="-" />
-    </div>
-    <div className="keys">
-      <CalcButton value="1" />
-      <CalcButton value="2" />
-      <CalcButton value="3" />
-      <CalcButton value="+" />
-    </div>
-    <div className="keys">
-      <CalcButton value="0" />
-      <CalcButton value="." />
-      <CalcButton value="=" />
-    </div>
-  </section>
-);
+const Calculator = () => {
+  const [calcObj, setCalcObj] = useState({ total: null, next: null, operation: null });
+
+  const handleButtonClick = (buttonName) => {
+    const newCalcObj = calculate(calcObj, buttonName);
+    setCalcObj(newCalcObj);
+  };
+
+  return (
+    <section className="calc-UI">
+      <div className="keys">
+        <input type="text" id="display" value={calcObj.next || calcObj.total || '0'} />
+      </div>
+      <div className="keys">
+        <CalcButton value="AC" onClick={() => handleButtonClick('AC')} />
+        <CalcButton value="+/-" onClick={() => handleButtonClick('+/-')} />
+        <CalcButton value="%" onClick={() => handleButtonClick('%')} />
+        <CalcButton value="&divide;" onClick={() => handleButtonClick('&divide;')} />
+      </div>
+      <div className="keys">
+        <CalcButton value="7" onClick={() => handleButtonClick('7')} />
+        <CalcButton value="8" onClick={() => handleButtonClick('8')} />
+        <CalcButton value="9" onClick={() => handleButtonClick('9')} />
+        <CalcButton value="x" onClick={() => handleButtonClick('x')} />
+      </div>
+      <div className="keys">
+        <CalcButton value="4" onClick={() => handleButtonClick('4')} />
+        <CalcButton value="5" onClick={() => handleButtonClick('5')} />
+        <CalcButton value="6" onClick={() => handleButtonClick('6')} />
+        <CalcButton value="-" onClick={() => handleButtonClick('-')} />
+      </div>
+      <div className="keys">
+        <CalcButton value="1" onClick={() => handleButtonClick('1')} />
+        <CalcButton value="2" onClick={() => handleButtonClick('2')} />
+        <CalcButton value="3" onClick={() => handleButtonClick('3')} />
+        <CalcButton value="+" onClick={() => handleButtonClick('+')} />
+      </div>
+      <div className="keys">
+        <CalcButton value="0" onClick={() => handleButtonClick('0')} />
+        <CalcButton value="." onClick={() => handleButtonClick('.')} />
+        <CalcButton value="=" onClick={() => handleButtonClick('=')} />
+      </div>
+    </section>
+  );
+};
 
 export default Calculator;
