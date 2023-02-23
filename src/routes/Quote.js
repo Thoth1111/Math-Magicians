b59a05fec28fff11aeb000876b03a038c5d8218c
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-23 08:13:32 +0300

    Fix eslint bug

--- src/routes/Quote.js
+++ src/routes/Quote.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Quote = () => (
   <div>
     <p>
-    "Mathematics is not about numbers, equations, computations, or algorithms:
-    it is about understanding." 
-    - William Paul Thurston
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding.
+      - William Paul Thurston
     </p>
   </div>
 );
--- src/routes/calculator.js
+++ src/routes/calculator.js
@@ -23,41 +23,41 @@ const Calculator = () => {
 
   return (
     <>
-    <h2>It's Math Time!!</h2>
-    <section className="calc-UI">
-      <div id="display">
-        {clickedBtns || 0}
-      </div>
-      <div className="keys">
-        <CalcButton value="AC" onClick={() => handleButtonClick('AC')} />
-        <CalcButton value="+/-" onClick={() => handleButtonClick('+/-')} />
-        <CalcButton value="%" onClick={() => handleButtonClick('%')} />
-        <CalcButton value="÷" onClick={() => handleButtonClick('÷')} />
-      </div>
-      <div className="keys">
-        <CalcButton value="7" onClick={() => handleButtonClick('7')} />
-        <CalcButton value="8" onClick={() => handleButtonClick('8')} />
-        <CalcButton value="9" onClick={() => handleButtonClick('9')} />
-        <CalcButton value="x" onClick={() => handleButtonClick('x')} />
-      </div>
-      <div className="keys">
-        <CalcButton value="4" onClick={() => handleButtonClick('4')} />
-        <CalcButton value="5" onClick={() => handleButtonClick('5')} />
-        <CalcButton value="6" onClick={() => handleButtonClick('6')} />
-        <CalcButton value="-" onClick={() => handleButtonClick('-')} />
-      </div>
-      <div className="keys">
-        <CalcButton value="1" onClick={() => handleButtonClick('1')} />
-        <CalcButton value="2" onClick={() => handleButtonClick('2')} />
-        <CalcButton value="3" onClick={() => handleButtonClick('3')} />
-        <CalcButton value="+" onClick={() => handleButtonClick('+')} />
-      </div>
-      <div className="keys">
-        <CalcButton value="0" onClick={() => handleButtonClick('0')} />
-        <CalcButton value="." onClick={() => handleButtonClick('.')} />
-        <CalcButton value="=" onClick={() => handleEqualsClick()} />
-      </div>
-    </section>
+      <h2>It&apos;s Math Time!!</h2>
+      <section className="calc-UI">
+        <div id="display">
+          {clickedBtns || 0}
+        </div>
+        <div className="keys">
+          <CalcButton value="AC" onClick={() => handleButtonClick('AC')} />
+          <CalcButton value="+/-" onClick={() => handleButtonClick('+/-')} />
+          <CalcButton value="%" onClick={() => handleButtonClick('%')} />
+          <CalcButton value="÷" onClick={() => handleButtonClick('÷')} />
+        </div>
+        <div className="keys">
+          <CalcButton value="7" onClick={() => handleButtonClick('7')} />
+          <CalcButton value="8" onClick={() => handleButtonClick('8')} />
+          <CalcButton value="9" onClick={() => handleButtonClick('9')} />
+          <CalcButton value="x" onClick={() => handleButtonClick('x')} />
+        </div>
+        <div className="keys">
+          <CalcButton value="4" onClick={() => handleButtonClick('4')} />
+          <CalcButton value="5" onClick={() => handleButtonClick('5')} />
+          <CalcButton value="6" onClick={() => handleButtonClick('6')} />
+          <CalcButton value="-" onClick={() => handleButtonClick('-')} />
+        </div>
+        <div className="keys">
+          <CalcButton value="1" onClick={() => handleButtonClick('1')} />
+          <CalcButton value="2" onClick={() => handleButtonClick('2')} />
+          <CalcButton value="3" onClick={() => handleButtonClick('3')} />
+          <CalcButton value="+" onClick={() => handleButtonClick('+')} />
+        </div>
+        <div className="keys">
+          <CalcButton value="0" onClick={() => handleButtonClick('0')} />
+          <CalcButton value="." onClick={() => handleButtonClick('.')} />
+          <CalcButton value="=" onClick={() => handleEqualsClick()} />
+        </div>
+      </section>
     </>
   );
 };
